5a41fbe33b27bd5165271467676acaa0b250d7d0
grafe <maximizhevsk@gmail.com>
2024-09-16 16:48:50 +0400

    increased by 4

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ public class Main {
 
 
         var dog = 8.0;
+        dog = dog + 4;
+
         var cat = 3.6;
+        cat = cat + 4;
+
         var paper = 763789;
+        paper = paper + 4;
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
