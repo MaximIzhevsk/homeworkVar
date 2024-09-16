7468626fdaf0f10fec5a9787b746f3fa5551059a
grafe <maximizhevsk@gmail.com>
2024-09-16 16:51:34 +0400

    task - 3

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
         var dog = 8.0;
         dog = dog + 4;
+        dog = dog - 3.5;
 
         var cat = 3.6;
         cat = cat + 4;
+        cat = cat - 1.6;
 
         var paper = 763789;
         paper = paper + 4;
+        paper = paper - 7639;
 
         System.out.println(dog);
         System.out.println(cat);
