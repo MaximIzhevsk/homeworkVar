e1fae5fbc66df985df81f31c40aeee97cafa957c
grafe <maximizhevsk@gmail.com>
2024-09-16 16:57:50 +0400

    task - 4

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,28 @@ public class Main {
     public static void main(String[] args) {
 
 
-        var dog = 8.0;
-        dog = dog + 4;
-        dog = dog - 3.5;
+//        var dog = 8.0;
+//        dog = dog + 4;
+//        dog = dog - 3.5;
+//
+//        var cat = 3.6;
+//        cat = cat + 4;
+//        cat = cat - 1.6;
+//
+//        var paper = 763789;
+//        paper = paper + 4;
+//        paper = paper - 7639;
+//
+//        System.out.println(dog);
+//        System.out.println(cat);
+//        System.out.println(paper);
 
-        var cat = 3.6;
-        cat = cat + 4;
-        cat = cat - 1.6;
-
-        var paper = 763789;
-        paper = paper + 4;
-        paper = paper - 7639;
-
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
 
 
